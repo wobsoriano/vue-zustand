98b282dbd26429ecca8ecbed6c9b3948d98de76e
Robert Soriano <sorianorobertc@gmail.com>
2022-02-26 10:06:06 -0800

    update bundler

---
+++ test/index.test.ts
@@ -0,0 +1,39 @@
+import create from '../src'
+import { test, expect } from 'vitest'
+
+interface BearState {
+  bears: number
+  increase: () => void
+  decrease: () => void
+}
+
+const useStore = create<BearState>((set) => ({
+  bears: 0,
+  increase: () => set((state) => ({ bears: state.bears + 1 })),
+  decrease: () => set((state) => ({ bears: state.bears - 1 })),
+}))
+
+test('returns default zustand properties', () => {
+  expect(typeof useStore.setState).toBe('function')
+  expect(typeof useStore.getState).toBe('function')
+  expect(typeof useStore.subscribe).toBe('function')
+  expect(typeof useStore.destroy).toBe('function')
+})
+
+test('increments', () => {
+  const bears = useStore((state) => state.bears)
+  const increase = useStore((state) => state.increase)
+  expect(bears.value).toBe(0)
+  increase.value()
+  increase.value()
+  expect(bears.value).toBe(2)
+})
+
+test('decrements', () => {
+  const bears = useStore((state) => state.bears)
+  const decrease = useStore((state) => state.decrease)
+  decrease.value()
+  expect(bears.value).toBe(1)
+  decrease.value()
+  expect(bears.value).toBe(0)
+})
