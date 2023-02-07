9445e4fec380d1e90281f3ee716b0eb0a33d3bfa
wobsoriano <sorianorobertc@gmail.com>
2023-02-07 10:20:59 -0800

    fix types

--- src/util.ts
+++ src/util.ts
@@ -4,7 +4,7 @@ import { computed, reactive, unref } from 'vue'
 export function refToReactive<O, K extends keyof O>(
   result: Ref<O>,
 ): O {
-  const keys = Object.keys(unref(result))
+  const keys = Object.keys(unref(result) as any)
   return reactive(
     Object.fromEntries(
       keys.map(key => [key, computed(() => result.value[key as K])]),
